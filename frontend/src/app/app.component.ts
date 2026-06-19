31c1399155b1b9b564a0573f311447c9b3f30ceb
epoirier-clement <epoirier-clement@reference.ca>
2026-06-19 14:23:16 -0400

    trace-3/page-accueil-basique

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,33 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { DrawingService } from './service/drawing.service';
-import { Drawing } from './model/drawing.model';
-import { DrawingViewComponent } from './drawing-view/drawing-view.component';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DrawingViewComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
-  drawings: Drawing[] = [];
-  loading = true;
-  error?: string;
-
-  constructor(private readonly drawingService: DrawingService) {}
-
-  ngOnInit(): void {
-    this.drawingService.getAll().subscribe({
-      next: (drawings) => {
-        this.drawings = drawings;
-        this.loading = false;
-      },
-      error: (err) => {
-        console.error(err);
-        this.error = "Couldn't reach the API. Is the backend running on :5126?";
-        this.loading = false;
-      },
-    });
-  }
-}
+export class AppComponent {}
