5a5419e79b8c65aec795c50a20cfc2c28fe9b687
epoirier-clement <epoirier-clement@reference.ca>
2026-06-30 11:17:52 -0400

    PIxel 16 Ajout login scalar

--- backend/PixelArt.Api/Program.cs
+++ backend/PixelArt.Api/Program.cs
@@ -13,7 +13,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer<BearerSecuritySchemeTransformer>();
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
